d1ee0d1d70f165694aa6d08aa315d3eb46671350
Matthew Duffy <matthewfduffy@gmail.com>
2021-07-16 08:27:32 -0500

    updating challenges 6 through 9

--- Basic_13_Challenges/06_print_average_of_array.py
+++ Basic_13_Challenges/06_print_average_of_array.py
@@ -1,3 +1,12 @@
 # analyze an array's values and print the average
 
 arr = [2, 6, 13, 15, 32, -9, 10]
+
+# Option 1 - without built-ins:
+sum = 0
+for num in arr:
+    sum += num
+
+print(sum / len(arr))
+
+
--- Basic_13_Challenges/08_square_array_values.py
+++ Basic_13_Challenges/08_square_array_values.py
@@ -1,1 +1,12 @@
 # Given an array, square each value in the array
+
+# Create Array
+arr = [n for n in range(1, 13)]
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+# Option 1 - using a new array:
+my_arr = []
+for num in arr:
+    my_arr.append(num * num)
+
+print(my_arr)
