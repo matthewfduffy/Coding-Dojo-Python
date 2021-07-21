b04a13e2e64ccc45d79d4ab191b76ec1eef140cf
Matthew Duffy <matthewfduffy@gmail.com>
2021-07-21 03:46:47 -0500

    adding solutions for challenge 11 and 12

--- Basic_13_Challenges/01_print1_to_255.py
+++ Basic_13_Challenges/01_print1_to_255.py
@@ -1,3 +1,3 @@
-# Print all integers from 1 to 255
+# 1. Print all integers from 1 to 255
 for i in range(1, 256):
     print(i)
--- Basic_13_Challenges/02_print_odds_1_to_255.py
+++ Basic_13_Challenges/02_print_odds_1_to_255.py
@@ -1,5 +1,8 @@
-# Print all odd integers from 1 to 255
+# 2. Print all odd integers from 1 to 255
 
+# Option 1 - For Loop:
 for i in range(1, 256):
     if i % 2 != 0:
         print(i)
+
+# Option 2 - Lambda:
